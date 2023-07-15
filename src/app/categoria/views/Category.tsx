271109aaa68e970505752c2e0845450ae10d338e
Jose Chavarria <chavarriaj84@gmail.com>
2023-07-15 19:25:43 -0400

    screen product and details

--- src/app/categoria/views/Category.tsx
+++ src/app/categoria/views/Category.tsx
@@ -6,44 +6,28 @@ import { useSearchParams } from 'next/navigation';
 import Loading from "@/components/Loading";
 import TitleSectionBorder from "@/components/TitleSectionBorder";
 
-const HoverableDiv = ({ handleMouseOver, handleMouseOut }: any) => {
-   return (
-      <>
-         <div className="md:container md:mx-auto p-4">
-            <div className="w-full gap-4 flex flex-wrap">
-               <Link onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} href={'/categoria'}><Image src="https://placehold.co/150x150/png" width={150} height={150} alt="Picture of the author" /></Link>
-            </div>
-         </div>
-      </>
-   );
-};
-
-const HoverText = () => {
-   return (
-      <div className="md:container md:mx-auto p-4">
-         <div className="w-full gap-4 flex flex-wrap">
-            <Link href={'/categoria'}><Image src="https://placehold.co/150x150/png" width={150} height={150} alt="Picture of the author" /></Link>
-         </div>
-      </div>
-   );
-};
-
-const LinkComponent = ({ data }: any) => {
-   console.log(data.length)
-   return data.length < 1 ? <>Sin subcategorias disponibles</> : data.map((item: any) => <Link className="flex flex-col items-center" key={item.id} href={'/categoria'}><Image src="https://placehold.co/150x150/png" width={150} height={150} alt="Picture of the author" />{item.attributes.nombre}</Link>)
-}
-
-const Category = () => {
+const LinkComponent = ({ subcategorias }: any) => {
    const [response, setResponse] = useState<any>([]);
    const [loading, setLoading] = useState<any>(true);
-   const paramSearch = useSearchParams();
-   const paramId = paramSearch.get('categoria') ?? 1;
+
+   const handleFetch = async (id: number) => {
+      try {
+         const api = new Api();
+         const dataProduct = await api.get(`https://www.dashboard.hauscenter.com.bo/api/productos?populate[imagen][fields][0]=name&populate[imagen][fields][1]=url&populate[images]&populate[subcategoria]=*&filters[subcategoria][id][$eq]=${id}`);
+         setResponse(dataProduct.data.data);
+         setLoading(false);
+      } catch (error) {
+         console.error('Error fetching data:', error);
+      }
+   }
+
    useEffect(() => {
       const fetchData = async () => {
          try {
             const api = new Api();
-            const data = await api.get(`https://www.dashboard.hauscenter.com.bo/api/categorias/${paramId}?populate=*`);
-            setResponse(data.data.data.attributes.subcategorias.data);
+            const dataProduct = await api.get(`https://www.dashboard.hauscenter.com.bo/api/productos?populate[subcategoria]=*&filters[subcategoria][id][$eq]=${subcategorias.id}`);
+            console.log(dataProduct)
+            setResponse(dataProduct.data.data);
             setLoading(false);
          } catch (error) {
             console.error('Error fetching data:', error);
@@ -52,11 +36,18 @@ const Category = () => {
 
       fetchData();
    }, []);
+
    return (
       <>
-         <div className="w-full gap-4 p-4 flex flex-wrap justify-start">
-            {loading ? <Loading /> : <LinkComponent data={response} />}
+         <div className="overflow-x-auto whitespace-nowrap scroll-category">
+            <div className="inline-block min-w-full">
+               {subcategorias.length < 1 ?
+                  <> Sin subcategorias disponibles </> :
+                  subcategorias.map((item: any) => <div  onClick={() => handleFetch(item.id)} key={item.id} className="inline-block cursor-pointer mx-4"><center><Image src="https://placehold.co/150x150/png" width={150} height={150} alt="Picture of the author" />{item.attributes.nombre}</center></div>
+                  )}
+            </div>
          </div>
+
          <div className="grid grid-cols-6 gap-16 mt-16 mx-4">
             <div className="md:col-span-2 col-span-6">
                <TitleSectionBorder title="Filtros" />
@@ -113,29 +104,70 @@ const Category = () => {
             <div className="md:col-span-4 col-span-6">
                <TitleSectionBorder title="Productos" />
 
-               <div className="grid grid-cols-6 mt-5 ">
-                  <Link href={'producto'} className="grid grid-cols-5 lg:col-span-4 col-span-6">
-                     <div className="md:col-span-2 col-span-5">
-                        <Image className="border-solid border-2 border-sky-900 rounded-3xl" src={'https://placehold.co/500x500/png'} width={700} height={700} style={{ width: '195px', height: '195px', objectFit: 'cover' }} alt="12" />
-                     </div>
-                     <div className="md:col-span-3 col-span-5 pt-4">
-                        <span className="upperacase text-[25px] font-bold text-sky-900 leading-6">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span><br />
-                        <span>- ANDROID 12 - 256GB ALMACENAMIENTO - 8 RAM + 3 RVIRTUAL - 5000 MAH</span>
-                     </div>
-                  </Link>
-                  <div className="lg:col-span-2 col-span-6 flex flex-col justify-between">
-                     <div className="text-right">
-                        <span className="upperacase text-[25px] font-bold text-sky-900">1000000</span>
-                        <span className="upperacase text-[25px] font-bold text-sky-900 uppercase text-right"> bs.</span>
-                     </div>
-                     <div className="flex justify-end">
-                        <button onClick={() => alert('love')}>❤</button>
-                        <button>🛍</button>
-                        <button>🛒</button>
-                     </div>
-                  </div>
-               </div>
+               {loading
+                  ? <>Cargando...</>
+                  : <CardProduct products={response} />
+               }
             </div>
+         </div>
+      </>
+
+   )
+}
+
+const CardProduct = ({ products }: any) => {
+   console.log(products)
+   return products.length < 1 ? <>Sin productos disponibles</> : products.map((element: any) =>
+      <div key={element.id} className="grid grid-cols-6 mt-5 ">
+
+         <Link key={element} href={{pathname: `/producto`,query: {producto: element.id}}} className="grid grid-cols-5 lg:col-span-4 col-span-6">
+            <div className="md:col-span-2 col-span-5 w-[195px] h-[195px] border-solid border-2 border-sky-900 flex justify-center items-center rounded-3xl">
+               <Image src={`https://www.dashboard.hauscenter.com.bo/${element.attributes.imagen.data[0].attributes.url}`} width={500} height={500} style={{ width: '140px', height: '140px', objectFit: 'contain' }} alt="12" />
+            </div>
+            <div className="md:col-span-3 col-span-5">
+               <span className="upperacase text-[25px] font-bold text-sky-900 leading-6">{element.attributes.nombre}.</span><br />
+               <span className="leading-3 text-sm">{element.attributes.descripcion_corta}</span>
+            </div>
+         </Link>
+         <div className="lg:col-span-2 col-span-6 flex flex-col justify-between">
+            <div className="text-right">
+               <span className="upperacase text-[25px] font-bold text-sky-900">{element.attributes.precio}</span>
+               <span className="upperacase text-[25px] font-bold text-sky-900 uppercase text-right"> bs.</span>
+            </div>
+            <div className="flex justify-end">
+               <button onClick={() => alert('love')}>❤</button>
+               <button>🛍</button>
+               <button>🛒</button>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+const Category = () => {
+   const [loadingSubcategoria, setLoadingSubcategoria] = useState<any>(true);
+   const [responseSubcategoria, setResponseSubcategoria] = useState<any>([]);
+   const paramSearch = useSearchParams();
+   const paramId = paramSearch.get('categoria');
+
+   useEffect(() => {
+      const fetchData = async () => {
+         try {
+            const api = new Api();
+            const data = await api.get(`https://www.dashboard.hauscenter.com.bo/api/categorias/${paramId}?populate=*`);
+            setResponseSubcategoria(data.data.data.attributes.subcategorias.data);
+            setLoadingSubcategoria(false);
+         } catch (error) {
+            console.error('Error fetching data:', error);
+         }
+      };
+
+      fetchData();
+   }, []);
+   return (
+      <>
+         <div className="w-full gap-4 p-4 flex flex-wrap justify-start">
+            {loadingSubcategoria ? <Loading /> : <LinkComponent subcategorias={responseSubcategoria} />}
          </div>
       </>
    )
--- src/app/producto/page.tsx
+++ src/app/producto/page.tsx
@@ -1,58 +1,85 @@
 'use client'
 import Image from "next/image";
 import LayoutProject from "../layout/layoutProyect";
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { Api } from "@/services/api";
+import { useState, useEffect } from "react";
+import Loading from "@/components/Loading";
+import ReactMarkdown from "react-markdown";
 
 export default function Home() {
   const router = useRouter();
+  const [loading, setLoading] = useState<any>(true);
+  const [response, setResponse] = useState<any>([]);
+  const [img, setImg] = useState('');
+  const paramSearch = useSearchParams();
+  const paramId = paramSearch.get('producto');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const api = new Api();
+        const data = await api.get(`https://www.dashboard.hauscenter.com.bo/api/productos/${paramId}?populate=*`);
+        console.log(data)
+        setResponse(data.data.data.attributes)
+        setImg(`https://www.dashboard.hauscenter.com.bo${data.data.data.attributes.imagen.data[0].attributes.url}`);
+        setLoading(false);
+        console.log(response)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <LayoutProject>
       <div className="grid grid-cols-6 mt-10 space-y-6">
-        <div className="lg:col-span-4 col-span-6">
-          <span className="text-4xl text-sky-900 font-extrabold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span> <br />
-          <span className="text-5xl text-sky-900 font-medium">SONY</span> <span className="text-3xl text-sky-900 font-medium">X80J</span>
-          <div className="grid grid-cols-3 gap-4">
-            <div className="lg:col-span-1 col-span-3 lg:block space-y-2 mt-10 flex flex-wrap justify-center gap-4">
-              <Image className="rounded-3xl border-2 border-solid border-sky-900" src={'https://placehold.co/500x500/png'} width={150} height={150} style={{margin: 0}} alt="" />
-              <Image className="rounded-3xl border-2 border-solid border-sky-900" src={'https://placehold.co/500x500/png'} width={150} height={150} style={{margin: 0}} alt="" />
-              <Image className="rounded-3xl border-2 border-solid border-sky-900" src={'https://placehold.co/500x500/png'} width={150} height={150} style={{margin: 0}} alt="" />
-            </div>
-            <div className="lg:col-span-2 col-span-3 flex justify-center"><Image src={'https://placehold.co/500x500/png'} width={500} height={500} alt="" /></div>
-          </div>
-        </div>
-        <div className="lg:col-span-2 col-span-6">
-          <div className="rounded-[60px] border-4 border-solid border-sky-900 p-10">
-            <p className="text-xl text-gray-600 line-through decoration-red-600"><span>0000</span> <span>BS.</span></p>
-            <p className="text-5xl text-sky-800 font-extrabold uppercase"><span>0000</span> Bs.</p>
-            <p className="my-2"><span className="text-sky-800 font-medium">ENVIÓ A DOMICILIO </span><span className="text-sky-800 font-extrabold">DISPONIBLE.</span></p>
-            <p className="border-2 border-solid border-sky-900"></p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptatem distinctio suscipit delectus veniam culpa neque incidunt dignissimos rerum est magnam sed aspernatur cupiditate, magni quia facilis pariatur ab doloremque!</p>
-            <div className="flex space-x-2 mt-4">
-              <select className="rounded-full border-2 border-solid border-sky-900 w-20 text-3xl text-center text-sky-900 font-bold" name="" id="">
-                <option value="1">1</option>
-              </select>
-              <button onClick={() => router.push('/lista-compra')} className="rounded-full border-2 border-solid border-sky-900 text-3xl text-center text-white font-bold bg-sky-900 px-5">Comprar</button>
-            </div>
-          </div>
-        </div>
-        <div className="col-span-6 border-t-2 border-solid border-sky-900 my-6">
-          <div className="grid grid-cols-2 my-10 space-y-6">
-            <div className="lg:col-span-1 col-span-6 border-r-2 border-solid border-sky-900 px-4">
-              <span className="text-3xl font-extrabold text-sky-900">CARACTERISTICAS</span>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, similique doloribus rem nulla laboriosam nisi optio, doloremque, non numquam dolor nam excepturi velit? Modi accusantium debitis deserunt, vel error explicabo.</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, similique doloribus rem nulla laboriosam nisi optio, doloremque, non numquam dolor nam excepturi velit? Modi accusantium debitis deserunt, vel error explicabo.</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, similique doloribus rem nulla laboriosam nisi optio, doloremque, non numquam dolor nam excepturi velit? Modi accusantium debitis deserunt, vel error explicabo.</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, similique doloribus rem nulla laboriosam nisi optio, doloremque, non numquam dolor nam excepturi velit? Modi accusantium debitis deserunt, vel error explicabo.</p>
+        {
+          loading ? <div className="col-span-6"><Loading /></div>
+            :
+            <>
+              <div className="lg:col-span-4 col-span-6">
+                <span className="text-4xl text-sky-900 font-extrabold">{response.nombre}</span> <br />
+                <span className="text-5xl text-sky-900 font-medium">{response.marca.data.attributes.nombre}</span> <span className="text-3xl text-sky-900 font-medium">{response.modelo.data.attributes.nombre}</span>
+                <div className="grid grid-cols-3 gap-4">
+                  <div className="lg:col-span-1 col-span-3 lg:block space-y-2 mt-10 flex flex-wrap justify-center gap-4">
+                    {response.imagen.data.map((element: any) => <div onClick={() => setImg(`https://www.dashboard.hauscenter.com.bo${element.attributes.url}`)} key={element.id} className="rounded-3xl border-2 border-solid border-sky-900 w-[150px] h-[150px] flex justify-center items-center"><Image src={`https://www.dashboard.hauscenter.com.bo${element.attributes.url}`} width={150} height={150} style={{ width: '100px', height: '100px', objectFit: 'contain' }} alt="" /></div>)}
+                  </div>
+                  <div className="lg:col-span-2 col-span-3 flex justify-center items-center"><Image src={img} width={500} height={500} style={{width: '350px', height: '350px', objectFit: 'contain'}} alt="" /></div>
+                </div>
+              </div>
+              <div className="lg:col-span-2 col-span-6">
+                <div className="rounded-[60px] border-4 border-solid border-sky-900 p-10">
+                  {response.precio_oferta > 0 ? <p className="text-xl text-gray-600 line-through decoration-red-600"><span>{response.precio_oferta}</span> <span>BS.</span></p> : <></>}
+                  <p className="text-5xl text-sky-800 font-extrabold uppercase"><span>{response.precio}</span> Bs.</p>
+                  <p className="my-2"><span className="text-sky-800 font-medium">ENVIÓ A DOMICILIO </span><span className="text-sky-800 font-extrabold">DISPONIBLE.</span></p>
+                  <p className="border-2 border-solid border-sky-900"></p>
+                  <p>{response.descripcion_corta}</p>
+                  <div className="flex space-x-2 mt-4">
+                    <select className="rounded-full border-2 border-solid border-sky-900 w-20 text-3xl text-center text-sky-900 font-bold" name="" id="">
+                      <option value="1">1</option>
+                    </select>
+                    <button onClick={() => router.push('/lista-compra')} className="rounded-full border-2 border-solid border-sky-900 text-3xl text-center text-white font-bold bg-sky-900 px-5">Comprar</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-6 border-t-2 border-solid border-sky-900 my-6">
+                <div className="grid grid-cols-2 my-10 space-y-6">
+                  <div className="lg:col-span-1 col-span-6 border-r-2 border-solid border-sky-900 px-4">
+                    <span className="text-3xl font-extrabold text-sky-900">CARACTERISTICAS</span>
+                    <ReactMarkdown>{response.caracteristica}</ReactMarkdown>
 
-            </div>
-            <div className="lg:col-span-1 col-span-6 px-4">
-              <span className="text-3xl font-extrabold text-sky-900">ESPECIFICACIONES</span>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, similique doloribus rem nulla laboriosam nisi optio, doloremque, non numquam dolor nam excepturi velit? Modi accusantium debitis deserunt, vel error explicabo.</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, similique doloribus rem nulla laboriosam nisi optio, doloremque, non numquam dolor nam excepturi velit? Modi accusantium debitis deserunt, vel error explicabo.</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, similique doloribus rem nulla laboriosam nisi optio, doloremque, non numquam dolor nam excepturi velit? Modi accusantium debitis deserunt, vel error explicabo.</p>
-            </div>
-          </div>
-        </div>
+                  </div>
+                  <div className="lg:col-span-1 col-span-6 px-4">
+                    <span className="text-3xl font-extrabold text-sky-900">ESPECIFICACIONES</span>
+                    <ReactMarkdown>{response.especificacion}</ReactMarkdown>
+                  </div>
+                </div>
+              </div>
+            </>
+        }
       </div>
     </LayoutProject>
   )
