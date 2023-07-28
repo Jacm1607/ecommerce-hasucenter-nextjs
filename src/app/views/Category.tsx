3ebbaab467252ad8c3b670555828fedbc7eeb66f
Jose Chavarria <chavarriaj84@gmail.com>
2023-07-27 21:14:52 -0400

    imagen de categorias

--- src/app/views/Category.tsx
+++ src/app/views/Category.tsx
@@ -12,7 +12,7 @@ const Category = () => {
      const fetchData = async () => {
        try {
         const api = new Api();
-         const data = await api.get('https://www.dashboard.hauscenter.com.bo/api/categorias');
+         const data = await api.get('https://www.dashboard.hauscenter.com.bo/api/categorias?populate=*');
          setLoading(false);
          setResponse(data.data);
        } catch (error) {
@@ -25,9 +25,9 @@ const Category = () => {
  
    return (
      <div>
-       <div className="w-full gap-4 p-4 flex flex-wrap justify-center">
+       <div className="w-full gap-14 p-4 flex flex-wrap justify-center">
        {
-        loading ? <Loading/> : response.data.map((item: any) => <Link className="flex flex-col items-center" key={item.id} href={{ pathname: '/categoria', query: { categoria: item.id },}}><Image src="https://placehold.co/150x150/png" width={150} height={150} alt="Picture of the author" />{item.attributes.nombre}</Link>)
+        loading ? <Loading/> : response.data.map((item: any) => <Link className="flex flex-col items-center text-sky-900 font-bold" key={item.id} href={{ pathname: '/categoria', query: { categoria: item.id },}}><Image src={`https://www.dashboard.hauscenter.com.bo/${item.attributes.imagen.data.attributes.url}`} width={150} height={150} style={{width:100, height:100, objectFit: 'contain'}} alt="Picture of the author" />{item.attributes.nombre}</Link>)
        }
        </div>
      </div>
