12576336a0153b0eb26b04163343997be17d4567
Jose <chavarriaj84@gmail.com>
2023-08-18 23:34:26 -0400

    añadio imagenes al header

--- src/components/HeaderOption.tsx
+++ src/components/HeaderOption.tsx
@@ -12,16 +12,16 @@ const HeaderOption = () => {
         <div className="flex flex-wrap justify-center lg:justify-end gap-2 ">
           <Link className="font-bold text-sky-800" href={'/'}>INICIO</Link>
           <div className="border-l-2 border-sky-800 h-full">&nbsp;</div>
-          <Link className="font-bold text-sky-800" href={'/'}>CATALOGO</Link>
-          <div className="border-l-2 border-sky-800 h-full">&nbsp;</div>
-          <Link className="font-bold text-sky-800" href={'/'}>CONTACTANOS</Link>
-          <div className="border-l-2 border-sky-800 h-full">&nbsp;</div>
-          <Link className="font-bold text-sky-800" href={'/'}>FAVORITOS</Link>
+          <Link className="font-bold text-sky-800" href={'https://cdn.hauscenter.com.bo/CATALOGO%20AGOSTO%20AL%20COSTO.pdf'}>CATALOGO</Link>
+          {/* <div className="border-l-2 border-sky-800 h-full">&nbsp;</div> */}
+          {/* <Link className="font-bold text-sky-800" href={'/'}>CONTACTANOS</Link> */}
+          {/* <div className="border-l-2 border-sky-800 h-full">&nbsp;</div> */}
+          {/* <Link className="font-bold text-sky-800" href={'/'}>FAVORITOS</Link> */}
         </div>
         <div className="flex flex-wrap justify-center lg:justify-end gap-2 my-4">
-          <Link href={'mi-cuenta'} className="uppercase py-1 px-4 border-2 border-sky-800 rounded-3xl text-sky-800 hover:bg-sky-800 hover:text-white font-semibold place-items-center flex justify-center gap-2 h-12 w-40 text-start text-sm"><Image src={'http://via.placeholder.com/464x130/png'} width={30} height={30} alt={""} /> Mi cuenta</Link>
-          <Button label="SEGUIR MI PEDIDO" />
-          <Button label="CREDI HAUS" />
+          <Link href={'mi-cuenta'} className="uppercase py-1 px-4 border-2 border-sky-800 rounded-3xl text-sky-800  font-semibold place-items-center flex justify-center gap-2 h-12 w-40 text-start text-sm"><Image src={'/btn-cuenta.png'} width={30} height={30} alt={""} /> Mi cuenta</Link>
+          {/* <Button label="SEGUIR MI PEDIDO" /> */}
+          <Link href={'mi-cuenta'} className="uppercase py-1 px-4 border-2 border-sky-800 rounded-3xl text-sky-800  font-semibold place-items-center flex justify-center gap-2 h-12 w-40 text-start text-sm"><Image src={'/btn-credito.png'} width={30} height={30} alt={""} /> CREDI HAUS</Link>
         </div>
       </div>
     </div>
