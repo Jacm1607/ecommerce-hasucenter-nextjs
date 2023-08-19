7105e69c85e173bad7ebd9d82d92178a5bd55f84
Jose <chavarriaj84@gmail.com>
2023-08-19 01:09:09 -0400

    info de producto destacado

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 const Feature = () => {
     return (
         <div className="grid grid-cols-2 w-full">
-            <div className="lg:col-span-1 col-span-2">
-                <Image src={"https://placehold.co/992x832/png"} width={992} height={832} style={{ width: '100%', height: '100%' }} alt="" />
+            <div className="lg:col-span-1 col-span-2 mt-20">
+                <Image src={'/iPhone-13.png'} width={992} height={832} style={{ width: '100%', height: '100%' }} alt="" />
             </div>
             <div className="lg:col-span-1 col-span-2 md:px-10 px-1">
                 <div className="">
@@ -18,7 +18,7 @@ const Feature = () => {
                 </div>
                 <div className="">
                     <p className="text-sky-900 text-[30px] uppercase">CARACTERÍSTICAS:</p>
-                    <p className="text-sky-900 text-[20px] uppercase md:px-8">- COLORES DISPONIBLES: AZUL, NEGRO, GOLD ROSE - ALMACENAMIENTO: 128GB / 256GB - PANTALLA AMOLED - PANTALLA DE 6 PULGADAS</p>
+                    <p className="text-sky-900 text-[20px] uppercase md:px-8">El iPhone 13 presenta un diseño similar al iPhone 12 con nuevos colores. Sus mejoras incluyen cámaras avanzadas, chip A15 Bionic para un rendimiento más rápido, pantallas OLED vibrantes, almacenamiento inicial de 128GB, conectividad 5G y funciones como Modo Cinemático y fotografía computacional.</p>
                 </div>
                 <center className="m-6"><Link className="md:text-[36px] text-white bg-sky-950 rounded-full px-8 py-2 font-bold tracking-wider" href={'/'}>ADQUIÉRELO YA</Link></center>
             </div>
