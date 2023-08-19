73e0bc4279885560f1efeaeb154f64e86b22fe99
Jose <chavarriaj84@gmail.com>
2023-08-18 23:50:15 -0400

    footer funcional con sucursales

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ import Link from "next/link"
         <div className="w-full">
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 bg-sky-900 pt-14 pb-4 px-4">
                 <div className="">
-                    <Image src={"http://via.placeholder.com/350x250"} width={350} height={250} alt="" />
+                    <Image src={'/hauscenter-blanco.png'} width={350} height={250} alt="" />
                 </div>
                 <div className="flex flex-col">
-                    <TitleFooter title='¿QUIÉNES SOMOS?'/>
+                    <TitleFooter title='¿QUIÉNES SOMOS?' img={'/quienes-somos.png'} />
                     <Link href={'/'} className="text-white text-xl">Acerca de</Link>
                     <Link href={'/'} className="text-white text-xl">Sucursales</Link>
                     <Link href={'/'} className="text-white text-xl">Sala de Prensa</Link>
@@ -17,19 +17,19 @@ import Link from "next/link"
                     <Link href={'/'} className="text-white text-xl">Trabaja con Nosotros</Link>
                 </div>
                 <div className="flex flex-col">
-                    <TitleFooter title='SERVICIO AL CLIENTE'/>
+                    <TitleFooter title='SERVICIO AL CLIENTE' img={'/servicio-cliente.png'}/>
                     <Link href={'/'} className="text-white text-xl">Términos y Condiciones</Link>
                     <Link href={'/'} className="text-white text-xl">Preguntas Frecuentes</Link>
                     <Link href={'/'} className="text-white text-xl">Centro de Ayuda</Link>
                 </div>
                 <div className="flex flex-col">
-                    <TitleFooter title='METODOS DE PAGO'/>
-                    <Link href={'/'} className="text-white text-xl">Código de QR</Link>
-                    <Link href={'/'} className="text-white text-xl">Pago Online</Link>
-                    <Link href={'/'} className="text-white text-xl">Transferencia Bancaria</Link>
-                    <Link href={'/'} className="text-white text-xl">Politica de Privacidad</Link>
-                    <TitleFooter title='PAGOS SEGUROS'/>
-                    <TitleFooter title='ENVIÓ A DOMICILIO'/>
+                    <TitleFooter title='Sucursales' img={'/quienes-somos.png'}/>
+                    <Link href={'https://goo.gl/maps/V5vMLQre2vnL4x298'} className="text-white text-xl">Central - Cañoto</Link>
+                    <Link href={'https://goo.gl/maps/Vt54zY5bczzdMpyp9'} className="text-white text-xl">Las Brisas</Link>
+                    <Link href={'https://goo.gl/maps/t3uDvZsbULxcHb3f9'} className="text-white text-xl">Mega Center</Link>
+                    <Link href={'https://goo.gl/maps/QkkrR6LniQWxURhk9'} className="text-white text-xl">Isabela La Católica</Link>
+                    <Link href={'https://goo.gl/maps/MUVLtJFfRsQ9xfXc8'} className="text-white text-xl">Ventura</Link>
+                    <Link href={'https://goo.gl/maps/XKfsctoNGVm4E2z38'} className="text-white text-xl">Cochabamba</Link>
                 </div>
             </div>
             <div className="w-full">
@@ -42,9 +42,9 @@ import Link from "next/link"
     )
 }
 
-const TitleFooter = ({title}: any) => 
+const TitleFooter = ({title, img}: any) => 
 <div className="flex items-center gap-4 my-2">
-    <Image src={"http://via.placeholder.com/50x50/png"} width={50} height={50} alt="" />
+    <Image src={img} width={50} height={50} alt="" />
     <span className="text-2xl font-extrabold text-white">{title}</span>
 </div>
 
